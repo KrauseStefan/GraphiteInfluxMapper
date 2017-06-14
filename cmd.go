8909acd5655683fc26f50aaee81e5830d6be25b2
krauseStefan <stefan.krausekjaer@gmail.com>
2017-06-14 21:39:44 +0200

    Added extra filed (users)

--- cmd.go
+++ cmd.go
@@ -1,14 +1,81 @@
 package main
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
+	"golang.org/x/text/transform"
+	"io"
 	"net"
 	"strings"
 	"time"
-	"io"
 )
 
-func readUntilClosed(reader io.Reader) error{
+var (
+	commonPrefix         = []byte("gatling.mysim.")
+	filterPrefix         = bytes.Join([][]byte{commonPrefix, []byte("users.")}, nil)
+	copyMeasurementPath  = bytes.Join([][]byte{commonPrefix, []byte("users.allUsers.active")}, nil)
+	copyMeasurementLabel = []byte("allActiveUsers")
+	previousTimestamp    = []byte("0000000000")
+)
+
+type filterTransformer struct {
+	transform.NopResetter
+
+	allActiveUsers []byte
+}
+
+func ExtractPathValues(line []byte) (path, value, timestamp []byte) {
+	if len(line) <= 1 {
+		return
+	}
+
+	parts := bytes.Split(line, []byte{' '})
+	path = parts[0]
+	value = parts[1]
+	timestamp = parts[2]
+	return
+}
+
+func (t filterTransformer) Transform(dst, src []byte, atEOF bool) (nDst, nSrc int, err error) {
+	var removedBytes = 0
+	origLines := bytes.Split(src, []byte{'\n'})
+	lines := [][]byte{}
+	for _, line := range origLines {
+		if !bytes.HasPrefix(line, filterPrefix) {
+			_, _, timeStamp := ExtractPathValues(line)
+
+			if len(timeStamp) > 2 && !bytes.Equal(timeStamp, previousTimestamp) {
+				copy(previousTimestamp, timeStamp)
+				lines = append(lines, bytes.Join([][]byte{
+					commonPrefix,
+					copyMeasurementLabel,
+					[]byte{' '},
+					t.allActiveUsers, //value
+					[]byte{' '},
+					timeStamp, //timestamp
+				}, nil))
+			}
+
+			lines = append(lines, line)
+		} else {
+			removedBytes += len(line) + 1
+			value := bytes.Split(bytes.TrimPrefix(line, filterPrefix), []byte{' '})[1]
+
+			if bytes.HasPrefix(line, copyMeasurementPath) {
+				t.allActiveUsers = value
+			}
+		}
+	}
+	nSrc = len(src)
+	nDst = copy(dst, bytes.Join(lines, []byte{'\n'}))
+	if nDst < (nSrc - removedBytes) {
+		err = errors.New("transform: short destination buffer")
+	}
+	return nDst, nSrc, err
+}
+
+func readUntilClosed(reader io.Reader) error {
 	bytesToRead := 4096
 	data := make([]byte, bytesToRead, bytesToRead)
 
@@ -37,7 +104,9 @@ func handleConnection(inConn net.Conn) error {
 	}
 	fmt.Printf("Proxying to %s\n", outConn.RemoteAddr())
 
-	inConnReader := io.TeeReader(inConn, outConn)
+	inConnTrans := transform.NewReader(inConn, filterTransformer{})
+
+	inConnReader := io.TeeReader(inConnTrans, outConn)
 	outConnReader := io.TeeReader(outConn, inConn)
 
 	go (func() {
